57e760fbb36c17f36473955b6f8a89800b85f05b
gigawa77 <rpamzn@gmail.com>
2024-05-07 09:21:13 +0100

    setup file and added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <img
+          src="https://st2.depositphotos.com/6789684/12262/v/450/depositphotos_122620866-stock-illustration-illustration-of-flat-icon.jpg"
+          className="logo"
+          width={100}
+        />
+        <br />
+        <button className="loginSignup">login/signup</button>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
