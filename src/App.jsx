29f75bac3332b100f4cf9d08745fcf90591f08d3
gigawa77 <rpamzn@gmail.com>
2024-05-07 13:18:03 +0100

    finished task 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import "./App.css";
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Articles from "./components/Articles";
+import OneArticle from "./components/OneArticle";
 
 function App() {
   const [article, setArticle] = useState([]);
 
   return (
-    <div>
+    <>
       <Header />
-      <Articles article={article} setArticle={setArticle} />
-    </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<Articles article={article} setArticle={setArticle} />}
+        />
+        <Route path="/:article_id" element={<OneArticle article={article} />} />
+      </Routes>
+    </>
   );
 }
 export default App;
